66c94a85da13a6a738e1d5638e2be14c94b437ab
joao achao <joao.achao@conecthus.org.br>
2023-02-07 14:47:43 -0400

    chore: add config .env

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Api Example')
-    .setDescription('The API description')
-    .setVersion('1.0')
+    .setTitle(process.env.API_TITLE)
+    .setDescription(process.env.API_DESCRIPTION)
+    .setVersion(process.env.API_VERSION)
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  SwaggerModule.setup(process.env.API_PATH, app, document);
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
