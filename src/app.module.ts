e670b6017d2e51ba883f3dadd9ded37a6c4a9cd1
joao achao <joao.achao@conecthus.org.br>
2023-02-07 15:07:44 -0400

    chore: add integration typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmOptions } from './database/database.providers';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmOptions),
+  ],
   controllers: [],
   providers: [],
 })
